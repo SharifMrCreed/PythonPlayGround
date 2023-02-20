7059e92edf529654b69e77caef5fe728704f606d
Sharif Ssemujju <sharifcreed@gmail.com>
2023-02-20 18:14:50 +0300

    snake now eats food

--- SnakeGame/SnakeGame.py
+++ SnakeGame/SnakeGame.py
@@ -8,8 +8,9 @@ SPACE_SIZE = 20
 FOOD_COLOR = "#FFFF00"
 SNAKE_COLOR = "#00FF00"
 SNAKE_LENGTH = 3
-DIRECTION = "down"
-GAME_SPEED = 24
+DIRECTION = "right"
+GAME_SPEED = 90
+score = 0
 
 
 class Snake:
@@ -18,11 +19,30 @@ class Snake:
         self.coordinates = []
         for length in range(SNAKE_LENGTH):
             self.coordinates.append([0, 0])
-        next_turn(self)
 
-    def move(self, direction):
+        for x, y in self.coordinates:
+            self.body_parts.append(
+                canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tags="snake")
+            )
+        global food
+        food = Food()
+        next_turn(self, food)
+
+    def move(self, direction, _food):
         # create a new head
+        global score
+        coincides = False
         xh, yh = self.coordinates[0]
+
+        if xh == _food.x_coordinate and yh == _food.y_coordinate:
+            score += 1
+            canvas.delete("food")
+            global food
+            food = Food()
+            draw_food(food)
+            coincides = True
+        else:
+            coincides = False
 
         if direction == "right":
             xh += SPACE_SIZE
@@ -32,38 +52,53 @@ class Snake:
             yh -= SPACE_SIZE
         else:
             yh += SPACE_SIZE
+
+        self.body_parts.insert(
+            0, canvas.create_rectangle(xh, yh, xh + SPACE_SIZE, yh + SPACE_SIZE, fill=SNAKE_COLOR, tags="snake")
+        )
+
+        if coincides is False:
+            self.coordinates.pop()
+            canvas.delete(self.body_parts[-1])
+            self.body_parts.pop()
         self.coordinates.insert(0, [xh, yh])
-        self.coordinates.pop(-1)
 
 
 class Food:
     def __init__(self):
-        x_coordinate = random.randint(0, SCREEN_WIDTH - SPACE_SIZE)
-        y_coordinate = random.randint(0, SCREEN_HEIGHT - SPACE_SIZE)
+        x_coordinate = random.randint(0, int(SCREEN_WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
+        y_coordinate = random.randint(0, int(SCREEN_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
 
 
-def draw_food():
-    food = Food()
-    canvas.create_oval(food.x_coordinate, food.y_coordinate,
-                       food.x_coordinate + SPACE_SIZE,
-                       food.y_coordinate + SPACE_SIZE,
-                       fill=FOOD_COLOR)
+def draw_food(_food):
+    canvas.create_oval(_food.x_coordinate, _food.y_coordinate,
+                       _food.x_coordinate + SPACE_SIZE,
+                       _food.y_coordinate + SPACE_SIZE,
+                       fill=FOOD_COLOR, tags="food")
 
 
-def draw_snake(_snake):
-    for x, y in _snake.coordinates:
-        canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tags="snake")
+def change_direction(direction):
+    global DIRECTION
+    if DIRECTION == "left" or DIRECTION == "right":
+        if direction != "left" and direction != "right":
+            DIRECTION = direction
+    else:
+        if direction != "up" and direction != "down":
+            DIRECTION = direction
 
 
-def next_turn(_snake):
-    _snake.move(DIRECTION)
-    draw_snake(_snake)
-    window.after(GAME_SPEED, next_turn, _snake)
+def next_turn(_snake, _food):
+    _snake.move(DIRECTION, _food)
+    window.after(GAME_SPEED, next_turn, _snake, _food)
 
 
 window = Tk()
+window.bind('<Left>', lambda event: change_direction("left"))
+window.bind('<Right>', lambda event: change_direction("right"))
+window.bind('<Up>', lambda event: change_direction("up"))
+window.bind('<Down>', lambda event: change_direction("down"))
 
 label = Label(text="Ssssnakeeeee", font=("Roboto", 28))
 label.pack()
@@ -72,6 +107,7 @@ canvas = Canvas(window, width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 canvas.pack()
 
 snake = Snake()
-draw_food()
+food = Food()
+draw_food(food)
 
 window.mainloop()
